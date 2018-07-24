74a184f2b29dd25623eba159ada65e9e18739de9
ijgomez <javier.izquierdo.gomez@gmail.com>
2018-07-24 08:02:37 +0200

    helloworld project

---
+++ test-helloworld/src/main/java/org/example/test/Helloworld.java
@@ -0,0 +1,21 @@
+package org.example.test;
+
+/**
+ * Type that prints <code>Hello World</code> when it is executed.
+ * 
+ * @author ijgomez
+ * 
+ */
+public class Helloworld {
+
+	/**
+	 * Main Method.
+	 * 
+	 * @param args
+	 *            Application Arguments.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World...");
+	}
+
+}
