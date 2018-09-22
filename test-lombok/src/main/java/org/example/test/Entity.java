e22ecb355559ee144771f99b17844d2a178ecb28
ijgomez <javier.izquierdo.gomez@gmail.com>
2018-09-22 10:59:32 +0200

    lombok project

---
+++ test-lombok/src/main/java/org/example/test/Entity.java
@@ -0,0 +1,12 @@
+package org.example.test;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class Entity {
+
+	private Long id;
+	
+}
---
+++ test-lombok/src/main/java/org/example/test/Main.java
@@ -0,0 +1,16 @@
+package org.example.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Entity e = new Entity();
+		
+		e.setId(1L);
+		
+		
+		System.out.println(e.getId());
+	}
+
+}
