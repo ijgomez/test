c37bffe034dba3b4c5873a8f31c22d45cf55ec1b
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2019-01-30 19:08:30 +0100

    remove course-extjs40

--- course-extjs40/04-DOM-js/04-DOM-js.js
+++
@@ -1,9 +0,0 @@
-Ext.onReady(function(){
-	var divCenter = document.getElementById("center");
-
-	var divNuevo = document.createElement("div");
-
-	divNuevo.innerHTML = "<p>Este texto se ñade de forma dinamica</p>";
-
-	divCenter.appendChild(divNuevo);
-});
--- course-extjs40/13-MVC/app/model/Book.js
+++
@@ -1,8 +0,0 @@
-Ext.define('Biblioteca.model.Book',{
-	extend: 'Ext.data.Model',
-	fields: [
-	         {name: 'id', type: 'int'},
-	         {name: 'titulo', type: 'string'},
-	         {name: 'autor', type: 'string'}
-	]
-});
