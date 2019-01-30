d800b6304409ef545be718ed778f63e98b22651c
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2019-01-30 19:12:10 +0100

    remove course

--- course-microservices/04-docker-microservice/README.md
+++
@@ -1,8 +0,0 @@
-# Docker Commands
-
-Run container:
-```sh
-$ docker run -p 8080:8080 ijgomez/course-docker-microservice
-```
-
-
--- course-microservices/09-clientes-microservice/src/main/java/org/example/test/dao/ClientesRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.test.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientesRepository extends JpaRepository<Cliente, Long>{
-
-}
