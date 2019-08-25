88edd2a04dab8a4134a365301ff8f6d4922c7147
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2019-08-25 22:24:18 +0200

    refactor

--- test-swing/src/main/java/org/example/test/Main.java
+++ test-swing/src/main/java/org/example/test/Main.java
@@ -1,63 +1,12 @@
 package org.example.test;
 
-import java.awt.Frame;
-
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-
+import org.example.test.views.Application;
 import org.example.test.views.ApplicationFrame;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class Main {
 
 	public static void main(String[] args) {
-		try {
-			try {
-//				 // set the brushed metal look and feel, if desired
-//				  System.setProperty("apple.awt.brushMetalLook", "true");
-//				  
-//				  // use the mac system menu bar
-//				  System.setProperty("apple.laf.useScreenMenuBar", "true");
-//				  
-//				  // set the "About" menu item name
-//				  System.setProperty("com.apple.mrj.application.apple.menu.about.name", "WikiStar");
-//				  
-//				  // use smoother fonts
-//				  System.setProperty("apple.awt.textantialiasing", "true");
-//
-//				  // ref: http://developer.apple.com/releasenotes/Java/Java142RNTiger/1_NewFeatures/chapter_2_section_3.html
-//				  System.setProperty("apple.awt.graphics.EnableQ2DX","true");
-//				  
-//				  // use the system look and feel
-//				  UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-
-	            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-	                if ("Nimbus".equals(info.getName())) {
-	                    UIManager.setLookAndFeel(info.getClassName());
-	                    break;
-	                }
-	            }
-	        } catch (Exception ex) {
-	            log.error("Setting look and feel failed:", ex);
-	        }
-
-
-			SwingUtilities.invokeLater(new Runnable() {
-				
-				public void run() {
-					ApplicationFrame frame = new ApplicationFrame(args);
-					frame.setVisible(true);
-					frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-					frame.toFront();
-				}
-			});
-
-        } catch(Exception e) {
-        	log.error("fail to start...", e);
-        }
-
+		Application.run(ApplicationFrame.class, args);
 	}
 
 }
---
+++ test-swing/src/main/java/org/example/test/views/Application.java
@@ -0,0 +1,64 @@
+package org.example.test.views;
+
+import java.awt.Frame;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+
+	public static void run(Class<ApplicationFrame> className, String... args) {
+		
+		SwingUtilities.invokeLater(() -> {
+			
+			try {
+//						 // set the brushed metal look and feel, if desired
+//						  System.setProperty("apple.awt.brushMetalLook", "true");
+//						  
+//						  // use the mac system menu bar
+//						  System.setProperty("apple.laf.useScreenMenuBar", "true");
+//						  
+//						  // set the "About" menu item name
+//						  System.setProperty("com.apple.mrj.application.apple.menu.about.name", "WikiStar");
+//						  
+//						  // use smoother fonts
+//						  System.setProperty("apple.awt.textantialiasing", "true");
+//
+//						  // ref: http://developer.apple.com/releasenotes/Java/Java142RNTiger/1_NewFeatures/chapter_2_section_3.html
+//						  System.setProperty("apple.awt.graphics.EnableQ2DX","true");
+//						  
+//						  // use the system look and feel
+//						  UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+	            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+	                if ("Nimbus".equals(info.getName())) {
+	                    UIManager.setLookAndFeel(info.getClassName());
+	                    break;
+	                }
+	            }
+	        } catch (Exception ex) {
+	            log.error("Setting look and feel failed:", ex);
+	        }
+			
+			try {
+				Constructor<ApplicationFrame> constructor = className.getConstructor(String[].class);
+				
+				ApplicationFrame frame = constructor.newInstance((Object)args);
+				frame.setVisible(true);
+				frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+				frame.toFront();
+			} catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
+					| IllegalArgumentException | InvocationTargetException e) {
+				log.error("fail to init app...", e);
+			}
+			
+		});
+
+      
+	}
+}
