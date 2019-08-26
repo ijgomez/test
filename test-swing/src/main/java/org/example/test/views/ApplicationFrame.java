7d9703d2855ca5aa70e9d068489ddfb7a1565f24
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2019-08-26 13:42:44 +0200

    refactor

--- test-swing/src/main/java/org/example/test/views/ApplicationFrame.java
+++ test-swing/src/main/java/org/example/test/views/ApplicationFrame.java
@@ -1,122 +1,52 @@
 package org.example.test.views;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-import javax.swing.JComponent;
 import javax.swing.JFileChooser;
-import javax.swing.JScrollPane;
 import javax.swing.filechooser.FileSystemView;
 
-import org.example.test.views.components.ApplicationConstants;
-import org.example.test.views.components.ApplicationModel;
 import org.example.test.views.components.ApplicationModelListener;
 import org.example.test.views.components.events.OpenFileEvent;
 import org.example.test.views.components.events.SaveFileEvent;
 import org.example.test.views.components.frames.AppFrame;
 import org.example.test.views.components.menubar.AppMenuBar;
 import org.example.test.views.components.toolbar.AppToolBar;
-import org.example.test.views.factories.ContainerViewFactory;
 import org.example.test.views.menu.ApplicationMenuBar;
 import org.example.test.views.toolbar.ApplicationToolBar;
 
-public class ApplicationFrame extends AppFrame implements ApplicationModelListener, ApplicationConstants {
+public class ApplicationFrame extends AppFrame implements ApplicationModelListener {
 
 	private static final long serialVersionUID = 4661793292792888017L;
 
 	private String[] arguments;
 
-	private ApplicationInitializationDialog initializationDialog;
-	
-	private AppMenuBar menuBar;
-	
-	private AppToolBar toolBar;
-	
-	private JComponent container;
-	
-	private ApplicationStatus status;
-	
 	public ApplicationFrame(String... args) {
 		super();
 		this.arguments = args;
 	}
 
-	protected void handlerInitializateGUI() {
-		JScrollPane scrollPane;
-		
-		this.menuBar = new ApplicationMenuBar();
-		
-		this.toolBar = new ApplicationToolBar();
-		
-		this.container = (JComponent) ContainerViewFactory.getInstance().getContainerView(null);
-		
-		this.status = new ApplicationStatus();
-		
-		scrollPane = new JScrollPane();
-		scrollPane.getViewport().add(this.container);
-		
-		super.setTitle(messagesResources.getString("application.title"));
-		super.setJMenuBar(this.menuBar);
-		super.setPreferredSize(MINIMUM_SIZE);
-		super.setMinimumSize(MINIMUM_SIZE);
-		super.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
-
-		super.getRootPane().setPreferredSize(MINIMUM_SIZE);
-		super.getRootPane().setMinimumSize(MINIMUM_SIZE);		
-		super.getRootPane().setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
-		
-		super.setLayout(new BorderLayout());
-		
-		super.add(toolBar, BorderLayout.NORTH);
-		super.add(scrollPane, BorderLayout.CENTER);
-		super.add(status, BorderLayout.SOUTH);
-		
-		// TODO Auto-generated method stub
-		
-		super.addWindowListener(new WindowAdapter() {
-			
-			@Override
-			public void windowOpened(WindowEvent e) {
-				initializationAction();
-			}
-			
-			@Override
-			public void windowClosing(WindowEvent e) {
-				confirmExitAction();
-			}
-		});
+	protected void handlerInitializateGUI() { }
+	
+	protected AppMenuBar buildMenuBar() {
+		return new ApplicationMenuBar();
 	}
 	
-	private void initializationAction() {
-		initializationDialog = new ApplicationInitializationDialog(this, arguments);
-		initializationDialog.setVisible(true);
-		initializationDialog.toFront();
+	protected AppToolBar buildToolBar() {
+		return new ApplicationToolBar();
+	}
+	
+	protected ApplicationStatus buildStatusBar() {
+		return new ApplicationStatus();
+	}
+	
+	protected ApplicationInitializationDialog buildInitializationAction() {
+		return new ApplicationInitializationDialog(this, arguments);
 	}
 
-	
-	
 	@Override
 	protected void handlerRegisterEvents() {
 		super.register(OpenFileEvent.class, (e) -> openFileDialog());
 		super.register(SaveFileEvent.class, (e) -> saveFileDialog());
 	}
-	
-	@Override
-	public void setModel(ApplicationModel model) {
-		super.setModel(model);
-		this.menuBar.setModel(model);
-		this.toolBar.setModel(model);
-		((ApplicationModelListener) this.container).setModel(model);
-		this.status.setModel(model);
-	}
 
-	protected void changeView(Class<?> classEntity) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 	private void openFileDialog() {
 		JFileChooser jfc;
 		
--- test-swing/src/main/java/org/example/test/views/components/frames/AppFrame.java
+++ test-swing/src/main/java/org/example/test/views/components/frames/AppFrame.java
@@ -1,21 +1,33 @@
 package org.example.test.views.components.frames;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.function.Consumer;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 
+import org.example.test.views.ApplicationInitializationDialog;
+import org.example.test.views.ApplicationStatus;
+import org.example.test.views.components.ApplicationConstants;
 import org.example.test.views.components.ApplicationModel;
 import org.example.test.views.components.ApplicationModelListener;
 import org.example.test.views.components.events.ApplicationEvent;
 import org.example.test.views.components.events.ChangeViewEvent;
 import org.example.test.views.components.events.CloseApplicationEvent;
 import org.example.test.views.components.helpers.LocaleHelper;
+import org.example.test.views.components.menubar.AppMenuBar;
+import org.example.test.views.components.toolbar.AppToolBar;
+import org.example.test.views.factories.ContainerViewFactory;
 import org.example.test.views.factories.ModalDialogFactory;
 
-public abstract class AppFrame extends JFrame implements ApplicationModelListener {
+public abstract class AppFrame extends JFrame implements ApplicationModelListener, ApplicationConstants {
 
 	private static final long serialVersionUID = 5279263641839891108L;
 
@@ -25,27 +37,84 @@ public abstract class AppFrame extends JFrame implements ApplicationModelListene
 	
 	private Map<Class<?>, Consumer<ApplicationEvent>> handlers = new HashMap<Class<?>, Consumer<ApplicationEvent>>();
 	
-	protected abstract void handlerInitializateGUI();
+	private ApplicationInitializationDialog initializationDialog;
 	
-	protected abstract void handlerRegisterEvents();
+	private AppMenuBar menuBar;
 	
+	private AppToolBar toolBar;
+	
+	private JComponent container;
+	
+	private ApplicationStatus status;
+	
+	/**
+	 * New Instance
+	 */
 	public AppFrame() {
 		this.initializateGUI();
 		this.registerEvents();
 	}
-	
+
 	private void initializateGUI() {
+		JScrollPane scrollPane;
+		
+		this.menuBar = buildMenuBar();
+		
+		this.toolBar = buildToolBar();
+		
+		this.container = (JComponent) ContainerViewFactory.getInstance().getContainerView(null);
+		
+		this.status = buildStatusBar();
+		
+		scrollPane = new JScrollPane();
+		scrollPane.getViewport().add(this.container);
+		
+		super.setTitle(messagesResources.getString("application.title"));
+		if (this.menuBar != null) {
+			super.setJMenuBar(this.menuBar);
+		}
+		super.setPreferredSize(MINIMUM_SIZE);
+		super.setMinimumSize(MINIMUM_SIZE);
+		super.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+
+		super.getRootPane().setPreferredSize(MINIMUM_SIZE);
+		super.getRootPane().setMinimumSize(MINIMUM_SIZE);		
+		super.getRootPane().setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+		
+		super.setLayout(new BorderLayout());
+		
+		super.add(toolBar, BorderLayout.NORTH);
+		super.add(scrollPane, BorderLayout.CENTER);
+		super.add(status, BorderLayout.SOUTH);
+		
 		// TODO Auto-generated method stub
+		
+		super.addWindowListener(new WindowAdapter() {
+			
+			@Override
+			public void windowOpened(WindowEvent e) {
+				initializationDialog = buildInitializationAction();
+				initializationDialog.setVisible(true);
+				initializationDialog.toFront();
+			}
+			
+			@Override
+			public void windowClosing(WindowEvent e) {
+				confirmExitAction();
+			}
+		});
 		this.handlerInitializateGUI();
 	}
 	
-	private void registerEvents() {
-		register(CloseApplicationEvent.class, (e) -> confirmExitAction());
-		register(ChangeViewEvent.class, (e) -> changeView(((ChangeViewEvent) e).getClassEntity()));
-		this.handlerRegisterEvents();
-	}
+	protected abstract void handlerInitializateGUI();
 	
-	protected abstract void changeView(Class<?> classEntity);
+	protected abstract ApplicationInitializationDialog buildInitializationAction();
+
+	protected abstract ApplicationStatus buildStatusBar();
+
+	protected abstract AppToolBar buildToolBar();
+
+	protected abstract AppMenuBar buildMenuBar();
 
 	@Override
 	public void setModel(ApplicationModel model) {
@@ -58,6 +127,10 @@ public abstract class AppFrame extends JFrame implements ApplicationModelListene
 			this.model.unregister(this);
 			this.model = model;
 		}
+		this.menuBar.setModel(model);
+		this.toolBar.setModel(model);
+		((ApplicationModelListener) this.container).setModel(model);
+		this.status.setModel(model);
 	}
 	
 	@Override
@@ -66,18 +139,31 @@ public abstract class AppFrame extends JFrame implements ApplicationModelListene
 			this.handlers.get(event.getClass()).accept(event);
 		}
 	}
-
+	
 	protected void register(Class<?> key, Consumer<ApplicationEvent> value) {
 		this.handlers.put(key, value);
 	}
 	
-	protected void confirmExitAction() {
+	private void registerEvents() {
+		register(CloseApplicationEvent.class, (e) -> confirmExitAction());
+		register(ChangeViewEvent.class, (e) -> changeView(((ChangeViewEvent) e).getClassEntity()));
+		this.handlerRegisterEvents();
+	}
+	
+	protected abstract void handlerRegisterEvents();
+
+	private void confirmExitAction() {
 		if (ModalDialogFactory.showConfirmExitDialog(this)) {
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			dispose();
 		} else {
 			setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		}
+	}
+	
+	private void changeView(Class<?> classEntity) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
