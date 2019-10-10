652474d461986fe0cf1c65925902e0560c20f9a6
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2019-10-10 19:33:09 +0200

    change log

--- test-ldap/option2/src/test/java/org/example/test/TestLdap.java
+++ test-ldap/option2/src/test/java/org/example/test/TestLdap.java
@@ -10,9 +10,7 @@ import org.junit.ClassRule;
 import org.junit.Test;
 
 /**
- * http://directory.apache.org/apacheds/advanced-ug/7-embedding-apacheds.html
- * https://cwiki.apache.org/confluence/display/DIRxSRVx10/Using+ApacheDS+for+unit+tests
- * https://www.adictosaltrabajo.com/2013/12/26/apache-d-s-embedded-ldap-server/
+ * 
  * @author ijgomez
  *
  */
